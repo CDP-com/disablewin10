23cb5719e207b09a5b71c51f7b9c62f98a62906a
smmadara <smmadara@gmail.com>
2016-04-08 13:03:53 -0400

    04-08-2016
    Updated Disable and Enable Batch files.

--- DisableW10Upgrade.bat
+++ DisableW10Upgrade.bat
@@ -6,13 +6,12 @@
 @echo off
 Setlocal EnableDelayedExpansion
 
-set "str=\windows\system32"
-set "strPath=%SystemDrive%%str%"
-
+set "str=\system32"
+set "strPath=%windir%%str%"
 
 tasklist /FI "IMAGENAME eq gwx.exe" 2>NUL | find /I /N "gwx.exe">NUL  
-if "%ERRORLEVEL%"=="0" taskkill /F /IM GWX.exe /T
-regedit /s "%~dp0\DisableW10Upgrade.reg"
+if "%ERRORLEVEL%"=="0" taskkill /F /IM GWX.exe /T > NUL
+regedit /s DisableW10Upgrade.reg
 
 for /r %strPath% %%a in (*) do (
     if /I "%%~nxa"=="gwx.exe" (
@@ -24,10 +23,13 @@ for /r %strPath% %%a in (*) do (
 if defined p (  
     if !pa:~-1!==\ SET pa=!pa:~0,-1!
     REM echo !pa!    
-    takeown /F %p% /A
-    ICACLS %p% /grant "*S-1-5-32-544:(D,WDAC)"
+    takeown /F %p% /A > NUL
+    ICACLS %p% /grant "*S-1-5-32-544:(D,WDAC)" >NUL
     ren %p% GWX1.exe
 )
+
+schtasks /query /TN "\Microsoft\Windows\Setup\gwx\launchtrayprocess" > NUL
+if "%ERRORLEVEL%"=="0" schtasks /change /TN "\Microsoft\Windows\Setup\gwx\launchtrayprocess" /DISABLE  > NUL
 
 Echo Windows 10 Update is disabled.
 
--- EnableW10Upgrade.bat
+++ EnableW10Upgrade.bat
@@ -1,11 +1,11 @@
 @echo off
 Setlocal EnableDelayedExpansion
 
-set "str=\windows\system32"
-set "strPath=%SystemDrive%%str%"
+set "str=\system32"
+set "strPath=%windir%%str%"
 
-reg delete HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\GWX /v DisableGWX /f
-regedit /s "%~dp0\EnableW10Upgrade.reg"
+reg delete HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\GWX /v DisableGWX /f > NULL
+regedit /s EnableW10Upgrade.reg
 
 for /r %strPath% %%a in (*) do (
     if /I "%%~nxa"=="gwx1.exe" (
@@ -17,10 +17,15 @@ for /r %strPath% %%a in (*) do (
 if defined p (  
     if !pa:~-1!==\ SET pa=!pa:~0,-1!
     REM echo !pa!    
-    takeown /F %p% /A
-    ICACLS %p% /grant "*S-1-5-32-544:(D,WDAC)"
+    takeown /F %p% /A > NUL
+    ICACLS %p% /grant "*S-1-5-32-544:(D,WDAC)" > NUL
     ren %p% GWX.exe
 )
+
+schtasks /query /TN "\Microsoft\Windows\Setup\gwx\launchtrayprocess" > NUL
+if "%ERRORLEVEL%"=="0" schtasks /change /TN "\Microsoft\Windows\Setup\gwx\launchtrayprocess" /ENABLE > NUL
+
+if exist "%strPath%\GWX\GWX.exe"  %strPath%\GWX\GWX.exe /tasklaunch
 
 Echo Windows 10 Update is enabled.
 
